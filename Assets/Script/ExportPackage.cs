75ed7f74420cb22360b3f0dc365aa5cf0bdd4a97
samezi <d_viper@hotmail.co.jp>
2016-12-27 20:40:23 +0900

    エクスポートスクリプトを設定

---
+++ Assets/Script/ExportPackage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class ExportPackage
+{
+    [MenuItem("Assets/Custom/Export Package")]
+    static void Export()
+    {
+        // エクスポート
+        AssetDatabase.ExportPackage("Assets/ExportPackage/Cube.prefab", "exportpackage.unitypackage",
+            ExportPackageOptions.Recurse | ExportPackageOptions.Default);
+    }
+}
